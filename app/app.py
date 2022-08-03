3068b2027856ff206261da6b4e593b80a9c55a62
Wade Martin <wademartin.dev@icloud.com>
2022-08-02 23:27:08 -0400

    amend filename & col names in csvs to match db, refactor app.py as requested

--- app/app.py
+++ app/app.py
@@ -1,5 +1,6 @@
 import csv
 import psycopg2
+from psycopg2 import sql
 import os
 from dotenv import load_dotenv
 
@@ -10,48 +11,51 @@ conn = None
 try:
     print('Connecting to the PostgreSQL database...')
     conn = psycopg2.connect(
-        host=os.getenv('DB_HOST'),
-        database=os.getenv('DB'),
-        user=os.getenv('DB_USER'),
-        password=os.getenv('DB_PASSWORD'),
-        port=os.getenv('DB_PORT')
+        host=os.getenv('POSTGRES_HOST'),
+        database=os.getenv('POSTGRES_DB'),
+        user=os.getenv('POSTGRES_USER'),
+        password=os.getenv('POSTGRES_PASSWORD'),
+        port=os.getenv('POSTGRES_PORT')
     )
 
     cur = conn.cursor()
 
-    files = ['ProductData.csv', 'SalesmanData.csv', 'SalesData.csv']
+    def get_files_with_ext(directory, file_ext=".csv"):
+        filenames = os.listdir(directory)
+        return [filename for filename in filenames if filename.endswith(file_ext)]
 
-    for file in files:
-        if file == 'ProductData.csv':
-            with open(file) as csv_file:
-                reader = csv.reader(csv_file)
-                next(reader)  # Skip the header row.
-                for row in reader:
-                    cur.execute(
-                        "INSERT INTO products (product_id, product_name, unit_price) VALUES (%s, %s, %s)",
-                        row
-                    )
-                    conn.commit()
-        elif file == 'SalesmanData.csv':
-            with open(file) as csv_file:
-                reader = csv.reader(csv_file)
-                next(reader)  # Skip the header row.
-                for row in reader:
-                    cur.execute(
-                        "INSERT INTO salespeople (salesperson_id, salesperson_name) VALUES (%s, %s)",
-                        row
-                    )
-                    conn.commit()
-        elif file == 'SalesData.csv':
-            with open(file) as csv_file:
-                reader = csv.reader(csv_file)
-                next(reader)  # Skip the header row.
-                for row in reader:
-                    cur.execute(
-                        "INSERT INTO sales (customer_name, salesperson_id, product_id, quantity_sold, sale_date) VALUES (%s, %s, %s, %s, %s)",
-                        row
-                    )
-                    conn.commit()
+    def read_file_and_insert_into_db(file, table_name):
+        with open(file) as csv_file:
+            reader = csv.reader(csv_file)
+            headers = next(reader)
+            for row in reader:
+                cur.execute(
+                    sql.SQL("INSERT INTO {} ({}) VALUES ({})").format(
+                        sql.Identifier(table_name),
+                        sql.SQL(', ').join(map(sql.Identifier, headers)),
+                        sql.SQL(', ').join(sql.Placeholder() * len(headers)),
+                    ), row
+                )
+        conn.commit()
+
+    def try_read_file(filename, files):
+        files.index(filename)
+        table_name = "products" if "Product" in filename else "salespeople" if "people" in filename else "sales"
+        read_file_and_insert_into_db(filename, table_name)
+
+    files = get_files_with_ext(os.getcwd())
+
+    try:
+        num_files = len(files)
+        if num_files != 3:
+            raise Exception("Expected three files, found %s files", num_files)
+        else:
+            try_read_file("ProductData.csv", files)
+            try_read_file("SalespeopleData.csv", files)
+            try_read_file("SalesData.csv", files)
+    except Exception as e:
+        print(e)
+
     cur.close()
 except (Exception, psycopg2.DatabaseError) as error:
     print(error)
